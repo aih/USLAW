db6a0b78769cd612e4a872607d8b84756d4f85a2
pi11 <userge@list.ru>
2013-08-05 03:05:36 +0400

    plugins info command/ small fix

--- plugins/management/commands/plugins_info.py
+++ plugins/management/commands/plugins_info.py
@@ -15,10 +15,10 @@ class Command(BaseCommand):
     help = """Display information about installed plugins"""
     def handle(self, *args, **options):
 
-        plugins = Plugin.objects.all()
+        plugins = Plugin.objects.filter().order_by("plugin_id")
         print "%s >>Plugins list: %s" % (Fore.RED, Fore.RESET)
         for p in plugins:
-            print "%s Plugin: %s %s %s Command: %s " % (Back.WHITE+Fore.BLACK, Style.BRIGHT+Fore.BLUE, p.name, Style.DIM+Fore.BLACK, p.plugin_command)
+            print "%s Plugin: %s %s %s Command: %s, ID: %s " % (Back.WHITE+Fore.BLACK, Style.BRIGHT+Fore.BLUE, p.name, Style.DIM+Fore.BLACK, p.plugin_command, p.plugin_id)
             if p.description:
                 print "   +-[Description]"+"-"*60+">"
                 print "   | ", p.description.replace("\n", "\n   | ")
