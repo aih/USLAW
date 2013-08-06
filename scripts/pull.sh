89eb137a0acccb84f7e64284c437a1b86a9be8c5
pi11 <userge@list.ru>
2013-08-07 00:39:33 +0400

    pull script fix for git

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-hg pull -u
-read -p "Reload UWSGI (y/n)?"
-[[ "$REPLY" == [yY] ]] && sudo /etc/init.d/uwsgi reload
+#hg pull -u
+#read -p "Reload UWSGI (y/n)?"
+#[[ "$REPLY" == [yY] ]] && sudo /etc/init.d/uwsgi reload
 
+git pull git@github.com:aih/USLAW development
 
