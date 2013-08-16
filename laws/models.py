385afcedd5be3ee033be1258833fc000c1d6c6d9
pi11 <userge@list.ru>
2013-08-17 01:20:11 +0400

    US tax Treaties model added

--- laws/models.py
+++ laws/models.py
@@ -1116,6 +1116,36 @@ class InternalRevenueBulletin(models.Model):
         return self.toc
 
 
+class Treties(models.Model):
+    """
+    """
+    #search = SphinxSearch() # optional: defaults to db_table
+    search = SphinxSearch('uslaw_treties')
+    title = models.CharField(max_length=400, null=True, blank=True)
+    document = models.FileField(blank=True, null=True, upload_to="uploads/documents/")
+    store = models.ForeignKey(TextStore, null=True, blank=True)
+    link = models.CharField(max_length=255, null=True, blank=True)
+    rate = models.IntegerField(default=0)
+
+    publication_date = models.DateTimeField(default=datetime.now())
+    is_active = models.BooleanField(default=True)
+    is_outdated = models.BooleanField(default=False)
+    external_publication_date = models.DateField(null=True, blank=True, max_length=20)
+    last_update = models.DateTimeField(blank=True, null=True)
+
+    def __unicode__(self):
+        return "%s" % (self.title)
+
+    def get_ext_date(self):
+        """Return external publication date"""
+        return self.external_publication_date
+
+    def get_name(self):
+        return self._meta.verbose_name_plural
+
+    class Meta:
+        verbose_name_plural = "U.S. Tax Treaties"    
+
 class PdfHash(models.Model):
     """We store all hashes for objects with PDF documents,
     so we can find duplicates"""
